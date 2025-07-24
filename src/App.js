da709f0d63857f9b7304465f44eb6f971e56acf5
thisisabukarsh <abdabukarsh@gmail.com>
2025-07-24 13:42:42 +0300

    synic

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from "./components/Header";
 function App() {
   return (
     <div className="">
-      <Header />
+      <h1>hello react</h1>
     </div>
   );
 }
--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-const Header = () => {
-  return (
-    <div>
-      <div className="text-4xl ">im a header</div>
-    </div>
-  );
-};
-
-export default Header;
