3ae93f8f7e565361f0aaad6cde33677d52cf8cd7
thisisabukarsh <155810138+thisisabukarsh@users.noreply.github.com>
2025-07-25 09:31:45 +0300

    footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="bg-slate-800 p-8 text-center text-white text-4xl">
+      <div>im a footer</div>
+    </div>
+  );
+};
+
+export default Footer;
