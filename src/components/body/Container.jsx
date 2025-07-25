7ac58e0cd2ddc29866718a2326972dd39d0b84e9
thisisabukarsh <155810138+thisisabukarsh@users.noreply.github.com>
2025-07-25 09:32:07 +0300

    we had made some changes on the style

--- src/components/body/Container.jsx
+++ src/components/body/Container.jsx
@@ -1,5 +1,7 @@
 const Container = ({ children }) => {
-  return <div className="mx-auto max-w-7xl">{children}</div>;
+  return (
+    <div className="mx-auto max-w-7xl flex flex-col gap-5 my-7">{children}</div>
+  );
 };
 
 export default Container;
